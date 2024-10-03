2e6400d456e9d5ee18219d6611fffc8dea4713e6
JinJeaJee <jpluemphiromnat@gmail.com>
2024-10-03 16:35:10 +0700

    dashboard api v1

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express"
+import dotenv from "dotenv"
+import helmet from "helmet"
+import morgan from "morgan"
+import bodyParser from "body-parser"
+import cors from "cors"
+import dashboardRouter from "./routes/dashboardRoutes"
+/* Route Import */
+
+
+/* Configulation */
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(cors())
+
+
+/* Routes */
+app.get("/check", (req,res)=> {
+    res.send("API INVENTORY IS RUNNING")
+})
+
+app.use("/dashboard", dashboardRouter)
+/* Server */
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`)
+})
