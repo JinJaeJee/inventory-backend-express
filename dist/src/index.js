2cc77fd37de83d6f68b915a4f10fd6e510167a6d
JinJeaJee <jpluemphiromnat@gmail.com>
2024-10-16 19:39:26 +0700

    server db add expenses

--- dist/src/index.js
+++ dist/src/index.js
@@ -9,9 +9,11 @@ const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
+/* Route Import */
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
-/* Route Import */
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const expenseRoutes_1 = __importDefault(require("./routes/expenseRoutes"));
 /* Configulation */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -28,6 +30,8 @@ app.get("/check", (req, res) => {
 });
 app.use("/dashboard", dashboardRoutes_1.default);
 app.use("/products", productRoutes_1.default);
+app.use("/users", userRoutes_1.default);
+app.use("/expenses", expenseRoutes_1.default);
 /* Server */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import helmet from "helmet"
 import morgan from "morgan"
 import bodyParser from "body-parser"
 import cors from "cors"
+
+/* Route Import */
 import dashboardRouter from "./routes/dashboardRoutes"
 import productRoutes from "./routes/productRoutes"
 import userRoutes from "./routes/userRoutes"
-/* Route Import */
-
+import expenseRoutes from "./routes/expenseRoutes";
 
 /* Configulation */
 
@@ -31,6 +32,7 @@ app.get("/check", (req,res)=> {
 app.use("/dashboard", dashboardRouter)
 app.use("/products", productRoutes)
 app.use("/users", userRoutes)
+app.use("/expenses", expenseRoutes); 
 /* Server */
 
 const port = process.env.PORT || 3001
